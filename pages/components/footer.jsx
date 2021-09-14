9deb666e7159178d924639bf60574c03a6ba7124
hammadkhokhar <hello@hammadkhokhar.com>
2021-09-14 17:35:58 +0500

    fixed footer

--- pages/components/footer.jsx
+++ pages/components/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="mt-12 lg:mt-18 sm:pb-36 sm:py-12 py-6">
+    <div className="mt-12 lg:mt-18 sm:pb-12 sm:py-12 py-6">
       <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
         <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
         <div className="flex flex-col justify-between lg:flex-row items-center">
