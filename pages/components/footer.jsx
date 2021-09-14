33633b2c5caa88904899acacb2ab1505bb1b1823
hammadkhokhar <hello@hammadkhokhar.com>
2021-09-14 20:01:44 +0500

    add email in footer

--- pages/components/footer.jsx
+++ pages/components/footer.jsx
@@ -5,6 +5,10 @@ const Footer = () => {
     <div className="mt-12 lg:mt-18 sm:pb-12 sm:py-12 py-6">
       <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
         <div className="pb-8 mb-2 border-t-2 border-gray-300 dark:border-white-300"></div>
+        <div className="text-sm mb-12 text-center">
+          <h2>General</h2><br/>
+          <h1 className="text-lg">hello@hammadkhokhar.com</h1>
+        </div>
         <div className="flex flex-col justify-between lg:flex-row items-center">
           <p>
             Theme from&nbsp;
