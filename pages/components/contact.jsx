cc09c99b9718c22b0594003728a7fe82b0033b92
hammadkhokhar <hello@hammadkhokhar.com>
2021-09-14 20:22:19 +0500

    contact page and component added

---
+++ pages/components/contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="container px-4 mx-auto">
+      <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
+        <div className="lg:px-4 lg:mt-12 ">
+          <h1 className="text-2xl font-bold text-gray-900 lg:text-2xl dark:text-white">
+            {/* Todo */}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,21 +7,36 @@ export default function Home() {
   return (
     <div className="space-y-14 lg:space-y-24">
       <Head>
-        
+        <title>Hire Hammad Khokhar</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="robots" content="follow, index" />
+        <meta
+          content="Hire Hammad Khokhar, he develops robust and scalable cloud-based web applications and REST APIs."
+          name="description"
+        />
+        <meta property="og:url" content={`https://hammadkhokhar.com`} />
+        <link rel="canonical" href={`https://hammadkhokhar.com`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Hammad Khokhar" />
+        <meta
+          property="og:description"
+          content="Hire Hammad Khokhar, he develops robust and scalable cloud-based web applications and REST APIs."
+        />
+        <meta property="og:title" content="Hire Hammad Khokhar" />
+        <meta property="og:image" content="/hammad-khokhar-avatar.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@hammadkhokhar" />
+        <meta name="twitter:title" content="Hire Hammad Khokhar" />
+        <meta
+          name="twitter:description"
+          content="Hire Hammad Khokhar, he develops robust and scalable cloud-based web applications and REST APIs."
+        />
+        <meta name="twitter:image" content="/hammad-khokhar-avatar.png" />
       </Head>
 
       <main className="max-w-4xl mx-auto mt-16 antialiased">
         <>
-          <About />
-          <div className="grid justify-items-center">
-            <FaRegArrowAltCircleDown
-              color="white"
-              title="AWS icon"
-              size="20px"
-              className="h-48 animate-pulse"
-            />
-          </div>
-          <Technologies />
+          
         </>
       </main>
     </div>
