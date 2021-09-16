0fa5bcfda88c61259aebdeb16d19921679f87559
hammadkhokhar <hello@hammadkhokhar.com>
2021-09-16 22:26:17 +0500

    added clients logo

--- pages/components/clients.jsx
+++ pages/components/clients.jsx
@@ -8,25 +8,38 @@ const Clients = () => {
       <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
         <div className="lg:px-4 lg:mt-12 mt-20 ">
           <h1 className="text-2xl font-bold text-gray-900 h-10 lg:text-2xl dark:text-white">
-            Worked with leading brands
+            Hired for leading brands
           </h1>
-          <div className="inline-flex items-center mr-2 pl-2 pr-2">
-            <Image
-              src="/huawei.png"
-              alt="Profile"
-              priority={true}
-              className="rounded-full mx-auto"
-              width={60}
-              height={60}
-            />
-            <SiIata
-              color="white"
-              title="Iata"
-              size="70px"
-              className="ml-10"
-            />
-            Agencies
+          <div className="inline-flex items-center pr-2">
+            <div className="ml-10">
+              <Image
+                src="/huawei.png"
+                alt="Profile"
+                priority={true}
+                className="rounded-full mx-auto"
+                width={60}
+                height={60}
+              />
             </div>
+            <div className="ml-10">
+              <SiIata
+                color="white"
+                title="Iata"
+                size="70px"
+              />
+              Agencies
+            </div>
+            <div className="ml-10">
+              <Image
+                src="/scb-logo-white.png"
+                alt="Profile"
+                priority={true}
+                className="mx-auto"
+                width={150}
+                height={60}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
