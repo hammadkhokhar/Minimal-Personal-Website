1745b67569560f2f83ce904af4685a39fd57befb
hammadkhokhar <hello@hammadkhokhar.com>
2021-09-17 01:24:09 +0500

    email API value changes

--- pages/api/email.tsx
+++ pages/api/email.tsx
@@ -5,23 +5,11 @@ sendgrid.setApiKey(process.env.SENDGRID);
 async function sendEmail(req, res) {
   try {
     await sendgrid.send({
-      to: "khokharhammad1@gmail.com", // Your email where you'll receive emails
-      from: "khokharhammad1@gmail.com", // your website email address here
+      to: "hello@hammadkhokhar.com", //verify your domain with sendgrid first
+      from: "hello@hammadkhokhar.com",
       subject: `[Lead from website] : ${req.body.subject}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
       <html lang="en">
-      <head>
-        <meta charset="utf-8">
-      
-        <title>The HTML5 Herald</title>
-        <meta name="description" content="The HTML5 Herald">
-        <meta name="author" content="SitePoint">
-      <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-      
-        <link rel="stylesheet" href="css/styles.css?v=1.0">
-      
-      </head>
-      
       <body>
         <div class="img-container" style="display: flex;justify-content: center;align-items: center;border-radius: 5px;overflow: hidden; font-family: 'helvetica', 'ui-sans';">              
               </div>
@@ -41,7 +29,7 @@ async function sendEmail(req, res) {
     return res.status(error.statusCode || 500).json({ error: error.message });
   }
 
-  return res.status(200).json({ error: "" });
+  return res.status(200).json({ error: "Unknown error occurred." });
 }
 
 export default sendEmail;
