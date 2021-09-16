7fb4bf7f1b2a39e5dd4dcf6a3325cffa3bf49f92
hammadkhokhar <hello@hammadkhokhar.com>
2021-09-16 18:17:30 +0500

    added client image section and fixed adjustment

--- pages/components/clients.jsx
+++ pages/components/clients.jsx
@@ -6,7 +6,7 @@ const Clients = () => {
   return (
     <div className="container px-4 mx-auto">
       <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
-        <div className="lg:px-4 lg:mt-12 ">
+        <div className="lg:px-4 lg:mt-12 mt-20 ">
           <h1 className="text-2xl font-bold text-gray-900 h-10 lg:text-2xl dark:text-white">
             Worked with leading brands
           </h1>
@@ -19,17 +19,11 @@ const Clients = () => {
               width={60}
               height={60}
             />
-            <SiEasyjet
-              color="white"
-              title="Iata"
-              size="70px"
-              className="mr-2"
-            />
             <SiIata
               color="white"
               title="Iata"
               size="70px"
-              className="ml-2"
+              className="ml-10"
             />
             Agencies
             </div>
