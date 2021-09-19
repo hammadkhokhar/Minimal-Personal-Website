9e4ca7e4747f3d99342670faa3c1aba04a2b11b7
hammadkhokhar <hello@hammadkhokhar.com>
2021-09-19 19:23:39 +0500

    hompage styling

--- pages/components/about.jsx
+++ pages/components/about.jsx
@@ -4,9 +4,11 @@ import Image from "next/image";
 const values = [
   "Cloud Computing",
   "Microservices",
+  "Web Applications",
   "AWS",
   "Serverless",
-  "Big Data",
+  "Data Science",
+  "eCommerce",
 ];
 
 const About = () => {
@@ -14,13 +16,13 @@ const About = () => {
     <div className="container px-4 mx-auto">
       <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
         <div className="lg:px-4 lg:mt-16 ">
-          <h1 className="text-2xl font-bold text-gray-900 lg:text-2xl dark:text-white">
+          <h1 className="text-lg font-bold text-gray-900 lg:text-2xl dark:text-white">
             Hey! I'm Hammad. Nice to meet you. <br />
             <br />
             You could have been anywhere in the world right now, but you're here
             with me. I appreciate that.
           </h1>
-          <div className="mt-6 text-gray-800 dark:text-white">
+          <div className="text-sm mt-6 text-gray-800 dark:text-white">
             <p className="mb-4">
               I develop robust and scalable cloud-based web applications and
               REST APIs.
@@ -28,7 +30,7 @@ const About = () => {
             {values.map((todo, index) => (
               <span
                 key={index}
-                className="inline-flex items-center mr-2 py-0.5 pl-2 pr-2 rounded-full text-xs font-medium bg-gray-50 text-gray-900"
+                className="inline-flex items-center mr-2 py-0.5 pl-2 pr-2 rounded-full text-xs font-medium bg-gray-50 text-gray-900 mb-1"
               >
                 {todo}
               </span>
